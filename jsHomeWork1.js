8098accbeeec58df1c32594c551ae04e0433da13
Yevhenii Mironenko <joe.mironenko@gmail.com>
2022-07-26 15:08:38 +0300

    \

---
+++ jsHomeWork1.js
@@ -0,0 +1,16 @@
+//1
+
+for(let i = 0; i<=100; i++) {
+    console.log (i);
+}
+
+//2
+for(let i = 0; i<=100; i++) {
+    if (i % 2== 0)
+    console.log (i);
+}
+
+//3
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 !== 0) continue;
+    console.log(i);}

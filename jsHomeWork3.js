d39fbd72833a2a8e8431de92683600572349734e
Yevhenii Mironenko <joe.mironenko@gmail.com>
2022-07-30 17:01:35 +0300

    3 ex

---
+++ jsHomeWork3.js
@@ -0,0 +1,15 @@
+let userName = prompt("Enter name!");
+switch (userName) {
+  case "admin":
+    alert("Hello, admin!");
+    break;
+  case "superAdmin":
+    alert("Hello, super admin!");
+    break;
+  case "moderator":
+    alert("Hello, moderator!");
+    break;
+  case "user":
+    alert("Hello, user!");
+    break;
+}
